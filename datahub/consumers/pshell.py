a655e7be330f13a51335a8dbd77100b3308af50b
gobbo_a <alexandre.gobbo@psi.ch>
2024-03-06 10:59:39 +0100

    Handle plot context

--- datahub/consumers/pshell.py
+++ datahub/consumers/pshell.py
@@ -27,16 +27,17 @@ except:
 
 
 class PShell(Consumer):
-    def __init__(self,  channels=None, address="localhost", port=7777, timeout=3.0, layout="Vertical", **kwargs):
+    def __init__(self,  channels=None, address="localhost", port=7777, timeout=3.0, layout="Vertical", context=None,  **kwargs):
         global PlotClient
         Consumer.__init__(self, **kwargs)
-        self.context = {}
+        self.clients = {}
         self.plots = {}
         self.address = address
         self.port = port
         self.timeout = timeout
         self.channels = channels
         self.layout = layout
+        self.context = context
 
         if PlotClient is None:
             # If PShell package not installed try to load the plot client from the URL
@@ -56,18 +57,16 @@ class PShell(Consumer):
             ps.close()
 
     def on_close(self):
-        for name, ps in self.context.items():
-            ps.close()
+        for name, client in self.clients.items():
+            client.close()
 
     def on_start(self, source):
-        #source_context=source.get_path().split("/")[-1].strip()
-        source_context = None
-        ps = PlotClient(address=self.address, port=self.port, context=source_context, timeout=self.timeout)
-        self.context[source] = ps
-        ps.clear_plots()
-        ps.set_context_attrs(quality=None, layout=self.layout)
-        ps.set_progress(None)
-        ps.set_status("Idle")
+        pc = PlotClient(address=self.address, port=self.port, context=self.context, timeout=self.timeout)
+        self.clients[source] = pc
+        pc.clear_plots()
+        pc.set_context_attrs(quality=None, layout=self.layout)
+        pc.set_progress(None)
+        pc.set_status("Idle")
 
     def on_stop(self, source, exception):
         pass
@@ -76,36 +75,36 @@ class PShell(Consumer):
         if self.channels:
             if not name in self.channels:
                 return
-        ps = self.context[source]
+        pc = self.clients[source]
         if len(shape)==0:
-            plot = ps.add_line_plot(name)
-            ps.clear_plot(plot)
-            series = ps.add_line_series(plot, name)
-            ps.set_line_series_attrs(series, color=None, marker_size=3, line_width=None, max_count=None)
+            plot = pc.add_line_plot(name)
+            pc.clear_plot(plot)
+            series = pc.add_line_series(plot, name)
+            pc.set_line_series_attrs(series, color=None, marker_size=3, line_width=None, max_count=None)
             xdata = None
         elif len(shape) == 1:
-            plot = ps.add_line_plot(name)
-            ps.clear_plot(plot)
-            series = ps.add_line_series(plot, name)
-            ps.set_line_series_attrs(series, color=None, marker_size=1, line_width=None, max_count=None)
+            plot = pc.add_line_plot(name)
+            pc.clear_plot(plot)
+            series = pc.add_line_series(plot, name)
+            pc.set_line_series_attrs(series, color=None, marker_size=1, line_width=None, max_count=None)
             xdata = list(range(shape[0]))
         elif len(shape) == 2:
-            plot = ps.add_matrix_plot(name, style=None, colormap="Viridis")
-            series = ps.add_matrix_series(plot, "Matrix Series 1")
-            ps.set_matrix_series_attrs(series, None, None, None, None, None, None)
+            plot = pc.add_matrix_plot(name, style=None, colormap="Viridis")
+            series = pc.add_matrix_series(plot, "Matrix Series 1")
+            pc.set_matrix_series_attrs(series, None, None, None, None, None, None)
             xdata = None
         else:
             _logger.warning("Unsupported shape for channel: " + name)
             return
-        ps.clear_series(series)
+        pc.clear_series(series)
         self.plots[name] = (plot, series, shape, xdata)
-        ps.set_status("Running")
-        ps.set_progress(0.5)
+        pc.set_status("Running")
+        pc.set_progress(0.5)
 
     def on_channel_record(self, source, name, timestamp, pulse_id, value):
         if name in self.plots:
             plot, series, shape, xdata = self.plots[name]
-            ps = self.context[source]
+            pc = self.clients[source]
             try:
                 if len(shape) == 0:
                     if type(timestamp) == str:
@@ -114,19 +113,20 @@ class PShell(Consumer):
                         value = float(value)
                     if isinstance(value, np.integer):  # Different scalar int types don't change header
                         value = int(value)
-                    ps.append_line_series_data(series, timestamp, value, None)
+                    pc.append_line_series_data(series, timestamp, value, None)
                 elif len(shape) == 1:
-                    ps.set_line_series_data(series, xdata, value.tolist(),  None)
+                    pc.set_line_series_data(series, xdata, value.tolist(),  None)
                 elif len(shape) == 2:
-                    ps.set_matrix_series_data(series, value.tolist(), None, None)
+                    pc.set_matrix_series_data(series, value.tolist(), None, None)
             except Exception as e:
                 print("Error in plotting: " + str(e))
 
 
     def on_channel_completed(self, source, name):
-        ps = self.context[source]
-        ps.set_progress(None)
-        ps.set_status("Done")
+        pc = self.clients.get(source, None)
+        if pc:
+            pc.set_progress(None)
+            pc.set_status("Done")
 
 
 
