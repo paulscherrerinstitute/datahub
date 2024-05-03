e2f4fb8dea62fa6745f56a9998f78f3be76b5ebe
alexgobbo <alexandre.gobbo@psi.ch>
2024-05-03 16:43:46 +0200

    New test

--- tests/test_daqbuf.py
+++ tests/test_daqbuf.py
@@ -1,8 +1,10 @@
+import time
 import unittest
 from datahub import *
 
 backend = "sf-databuffer"
 filename = "/Users/gobbo_a/dev/back/daqbuf.h5"
+filename = "/Users/gobbo_a/tst.h5"
 
 channels = ["S10BC01-DBPM010:Q1", "S10BC01-DBPM010:X1"]
 start = "2024-02-15T12:41:00Z"
@@ -35,6 +37,17 @@ class DataBufferTest(unittest.TestCase):
                 dataframe_json.reindex(sorted(dataframe_json.columns), axis=1)
                 print(dataframe_json)
             self.assertEqual(dataframe_cbor.equals(dataframe_json), True)
+
+    def test_save(self):
+        s = time.time()
+        with Daqbuf(backend=backend, cbor=True, parallel=True) as source:
+            hdf5 = HDF5Writer(filename)
+            source.add_listener(hdf5)
+            channels = ["S10BC01-DBPM010:Q1", "S10BC01-DBPM010:X1"]
+            start = "2024-05-02 09:00:00"
+            end = "2024-05-02 10:00:00"
+            source.req(channels, start, end)
+        print (time.time()-s)
 
 if __name__ == '__main__':
     unittest.main()
